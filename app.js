fa1c95dc8ae02e115fae9df35568e2e49cf9db68
Gurminder Singh <gurminder037@gmail.com>
2020-05-31 15:11:50 -0700

    Tracalorie-  First Commit

---
+++ app.js
@@ -0,0 +1,144 @@
+// Storage Controller
+const storageCtrl = function(){
+
+}();
+
+
+// Item Controller
+const ItemCtrl = function(){
+    // Item Constructor
+    const Item = function(id, name, calories){
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    const data = {
+         items: [
+            {id: 0, name: "Steak Dinner", calories: 1200 },
+            {id: 1, name: "Cookies", calories: 200 },
+            {id: 2, name: "Eggs", calories: 300 }
+        ],
+        currentItem: null,
+        totalCalories: 0        
+    }
+
+    // Public Methods
+    return {
+        getItems: function(){
+
+            return data.items;
+        },
+        addItem: function(name, calories){
+        let ID;
+        // create ID
+        if(data.items.length > 0){
+            ID = data.items[data.items.length - 1].id +1;
+        }else{
+            ID = 0;
+        }
+
+        // Calories to number
+        calories = parseInt(calories);
+
+        // Create new Item
+        newItem = new Item(ID, name, calories);
+
+        // Add to items array
+        data.items.push(newItem);
+
+        return newItem;
+        },
+        logData: function(){
+            return data;
+        }
+    }  
+ 
+}();
+
+// UI Controller
+const UICtrl = function(){
+    const UISelectors  = {
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories'
+    }
+
+
+    // Public Methods 
+    return{
+        populateItemList: function(items){
+            let html = '';
+
+            items.forEach(function(item){
+                html += `
+                <li class="collection-item" id="item-${item.id}">
+                    <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="edit-item fa fa-pencil"></i>
+                    </a>
+                </li>
+                `;
+            });
+
+            // Insert List Items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput: function(){
+            return {
+                name:document.querySelector(   UISelectors.itemNameInput).value,
+                calories:document.querySelector(UISelectors.itemCaloriesInput).value
+            }
+        },
+        getSelectors: function(){
+            return UISelectors;
+        }
+    } 
+
+}();
+
+// App Controller
+const App = function(UICtrl,ItemCtrl){
+    // Load event listeners
+    const loadEventListeners = function(){
+    // Get UI selectors
+    const UISelectors = UICtrl.getSelectors();
+    // Add item event
+    document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit )
+    }
+
+    // Add item submit
+    const itemAddSubmit = function(e){
+        // Get form Input from UI controller
+        const input = UICtrl.getItemInput();
+        
+        // Check for name and calories
+        if(input.name !== '' && input.calories !== ''){
+            const newItem = ItemCtrl.addItem( input.name, input.calories);
+        }else{
+
+        }
+
+        e.preventDefault();
+    }
+
+    // Public Methods
+    return {
+        init:function(){
+        console.log("Initializing App....");
+        
+        // Fetch Items from data structure
+        const items = ItemCtrl.getItems();
+
+        // Populate list with Items
+        UICtrl.populateItemList(items);
+        
+        // Load Event Listeners
+        loadEventListeners();
+        }             
+    }
+}(UICtrl,ItemCtrl);
+
+// Initializing App
+App.init();
